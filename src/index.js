c1d145c48b18bc8bf232683838d766fe078da090
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2024-01-10 08:56:24 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { books } = require('../data/library');
+
+// Requisito 1
+const fantasyOrScienceFiction = () => {
+  // escreva seu código aqui
+};
+
+// Requisito 2
+const oldBooksOrdered = (year) => {
+  // escreva seu código aqui
+};
+
+// Requisito 3
+const booksByAuthorBirthYear = (birthYear) => {
+  // escreva seu código aqui
+};
+
+// Requisito 4
+const fantasyOrScienceFictionAuthors = () => {
+  // escreva seu código aqui
+};
+
+// Requisito 5
+const oldBooks = (year) => {
+  // escreva seu código aqui
+};
+
+// Requisito 6
+const authorWith3DotsOnName = () => {
+  // escreva seu código aqui
+};
+
+module.exports = {
+  fantasyOrScienceFiction,
+  oldBooksOrdered,
+  booksByAuthorBirthYear,
+  fantasyOrScienceFictionAuthors,
+  oldBooks,
+  authorWith3DotsOnName,
+};
